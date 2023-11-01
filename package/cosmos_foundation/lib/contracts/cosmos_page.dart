78a957807f9a018e36a3ef5a6221a3b10e62d5e4
Renato Urrea <usuaryrenato@hotmail.com>
2023-11-01 14:31:26 -0700

    Cosmos UI building API Improved

--- package/cosmos_foundation/lib/contracts/cosmos_page.dart
+++ package/cosmos_foundation/lib/contracts/cosmos_page.dart
@@ -1,11 +1,7 @@
-import 'package:cosmos_foundation/models/options/route_options.dart';
 import 'package:flutter/material.dart';
 
 abstract class CosmosPage extends StatelessWidget {
-  final RouteOptions routeOptions;
-
-  const CosmosPage(
-    this.routeOptions, {
+  const CosmosPage({
     super.key,
   });
 }
--- package/cosmos_foundation/lib/contracts/cosmos_screen.dart
+++ package/cosmos_foundation/lib/contracts/cosmos_screen.dart
@@ -1,6 +1,13 @@
-
 import 'package:flutter/material.dart';
 
+/// UI Screen provides a mechanism to wrap a collection of UI Page into a design that doesn't change along the routing of the collection.
 abstract class CosmosScreen extends StatelessWidget {
-  const CosmosScreen({super.key});
+  /// The current UI Page resolved by the router manager.
+  final Widget page;
+
+  /// A new [CosmosScreen] contract configuration.
+  const CosmosScreen({
+    super.key,
+    required this.page,
+  });
 }
