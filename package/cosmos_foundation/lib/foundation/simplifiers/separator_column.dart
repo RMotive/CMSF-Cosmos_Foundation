914ac605793ceeb58452e3e641e6dbb6697a64b0
Renato Urrea <usuaryrenato@hotmail.com>
2023-11-01 18:45:06 -0700

    simplifier column separator

---
+++ package/cosmos_foundation/lib/foundation/simplifiers/separator_column.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SeparatorColumn extends StatelessWidget {
+  final CrossAxisAlignment crossAxisAlignment;
+  final MainAxisAlignment mainAxisAlignment;
+  final VerticalDirection verticalDirection;
+  final MainAxisSize mainAxisSize;
+  final TextBaseline? textBaseline;
+  final TextDirection? textDirection;
+  final List<Widget> children;
+  final bool includeStart;
+  final bool includeEnd;
+  final double spacing;
+  const SeparatorColumn({
+    super.key,
+    required this.children,
+    required this.spacing,
+    this.textBaseline,
+    this.textDirection,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.verticalDirection = VerticalDirection.down,
+    this.includeEnd = false,
+    this.includeStart = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: crossAxisAlignment,
+      mainAxisAlignment: mainAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      textBaseline: textBaseline,
+      textDirection: textDirection,
+      verticalDirection: verticalDirection,
+      children: <Widget>[
+        if (includeStart)
+          SizedBox.square(
+            dimension: spacing,
+          ),
+        for (Widget item in children) ...<Widget>{
+          if (!(children.indexOf(item) == 0))
+            SizedBox.square(
+              dimension: spacing,
+            ),
+          item,
+        },
+        if (includeEnd)
+          SizedBox.square(
+            dimension: spacing,
+          )
+      ],
+    );
+  }
+}
