38a327e9db5b3aae826964133ebbbfb21ac32d72
Renato Urrea <usuaryrenato@hotmail.com>
2024-01-15 16:49:59 -0800

    improved page building and renderizer

--- package/cosmos_foundation/lib/contracts/cosmos_page.dart
+++ package/cosmos_foundation/lib/contracts/cosmos_page.dart
@@ -4,4 +4,21 @@ abstract class CosmosPage extends StatelessWidget {
   const CosmosPage({
     super.key,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.sizeOf(context);
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minWidth: screenSize.width,
+        minHeight: screenSize.height,
+      ),
+      child: SingleChildScrollView(
+        child: compose(),
+      ),
+    );
+  }
+
+  Widget compose();
 }
