b1485053b6c9ac8ba88fad301efd6f39937bd62d
Renato Urrea <usuaryrenato@hotmail.com>
2024-01-21 11:49:36 -0800

    added a get method simplified api

--- package/cosmos_foundation/lib/contracts/cosmos_service.dart
+++ package/cosmos_foundation/lib/contracts/cosmos_service.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cosmos_foundation/models/structs/cosmos_uri_struct.dart';
 
 abstract class CosmosService {
   late final CosmosUriStruct endpoint;
+  late final HttpClient comm;
 
   CosmosService(CosmosUriStruct host, String servicePath) {
     endpoint = CosmosUriStruct.includeEndpoint(host, servicePath);
+    comm = HttpClient();
+  }
+
+  Future<TOutput> get<TOutput>(String path) async {
+    Uri url = endpoint.generateUri();
+    HttpClientRequest request = await comm.get(url.host, url.port, url.path);
+    HttpClientResponse response = await request.close();
+    String rawResponse = await response.transform(utf8.decoder).join();
+    TOutput decodedResponse = jsonDecode(rawResponse);
+    return decodedResponse;
   }
 }
