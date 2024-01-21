3546f9b5270a61e2dc4c13ed8fc6726fb8d6b982
Renato Urrea <usuaryrenato@hotmail.com>
2024-01-21 14:04:54 -0800

    improved http manager and communication

--- package/cosmos_foundation/lib/contracts/cosmos_service.dart
+++ package/cosmos_foundation/lib/contracts/cosmos_service.dart
@@ -1,22 +1,22 @@
 import 'dart:convert';
-import 'dart:io';
+import 'package:http/http.dart';
 
 import 'package:cosmos_foundation/models/structs/cosmos_uri_struct.dart';
 
 abstract class CosmosService {
   late final CosmosUriStruct endpoint;
-  late final HttpClient comm;
+  late final Client comm;
 
   CosmosService(CosmosUriStruct host, String servicePath) {
     endpoint = CosmosUriStruct.includeEndpoint(host, servicePath);
-    comm = HttpClient();
+    comm = Client();
   }
 
   Future<TOutput> get<TOutput>(String path) async {
     Uri url = endpoint.generateUri();
-    HttpClientRequest request = await comm.get(url.host, url.port, url.path);
-    HttpClientResponse response = await request.close();
-    String rawResponse = await response.transform(utf8.decoder).join();
+    Response request = await comm.get(url);
+    if (request.statusCode != 200) throw 'Wrong server answer';
+    String rawResponse = request.body;
     TOutput decodedResponse = jsonDecode(rawResponse);
     return decodedResponse;
   }
