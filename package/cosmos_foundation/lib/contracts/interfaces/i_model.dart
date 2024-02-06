f2fa9684ce2ff070f621815e9b796b477e4b1582
Renato Urrea <usuaryrenato@hotmail.com>
2024-02-05 21:27:22 -0800

    updated modelling interfaces

--- package/cosmos_foundation/lib/contracts/interfaces/i_model.dart
+++ package/cosmos_foundation/lib/contracts/interfaces/i_model.dart
@@ -1,7 +1,3 @@
-import 'package:cosmos_foundation/contracts/interfaces/i_scheme.dart';
-
-abstract interface class ModelInterface<TScheme extends IScheme> extends IModel {
-  TScheme generateScheme();
+abstract interface class IModel<T> {
+  Map<String, dynamic> toJson();
 }
-
-abstract interface class IModel {}
--- package/cosmos_foundation/lib/contracts/interfaces/i_scheme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cosmos_foundation/contracts/interfaces/i_model.dart';
-
-abstract interface class SchemeInterface<TModel extends IModel> extends IScheme {
-  TModel generateModel();
-  Map<String, dynamic> toJson();
-}
-
-abstract interface class IScheme {}
