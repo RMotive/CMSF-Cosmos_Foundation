80c2db37e7eac6b34ad2c54192405b10cae2b5c8
Renato Urrea <usuaryrenato@hotmail.com>
2024-02-05 21:12:18 -0800

    added an interface method to handle toJson object convertion

--- package/cosmos_foundation/lib/contracts/interfaces/i_scheme.dart
+++ package/cosmos_foundation/lib/contracts/interfaces/i_scheme.dart
@@ -2,6 +2,7 @@ import 'package:cosmos_foundation/contracts/interfaces/i_model.dart';
 
 abstract interface class SchemeInterface<TModel extends IModel> extends IScheme {
   TModel generateModel();
+  Map<String, dynamic> toJson();
 }
 
 abstract interface class IScheme {}
