f8b28261cd325f533544d68e3b2da98f3f65df87
Renato Urrea <usuaryrenato@hotmail.com>
2024-02-13 20:21:12 -0800

    added the constant constructor for model base contracts

--- package/cosmos_foundation/lib/contracts/bases/model_base.dart
+++ package/cosmos_foundation/lib/contracts/bases/model_base.dart
@@ -3,6 +3,8 @@ import 'package:cosmos_foundation/alias/aliases.dart';
 /// Generates a contract to handle model base abstraction that enforce the use
 /// of model tools to handle ease convertions.
 abstract class ModelBase {
+  const ModelBase();
+
   /// Converts the current object to a json object.
   JObject toJson();
 
