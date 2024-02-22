b02929810ea9303e545a91a6b46a675c78b10d43
Renato Urrea <usuaryrenato@hotmail.com>
2024-02-22 07:58:59 -0800

    improved control states evaluators

--- package/cosmos_foundation/lib/enumerators/evaluators/cosmos_control_states_evaluator.dart
+++ package/cosmos_foundation/lib/enumerators/evaluators/cosmos_control_states_evaluator.dart
@@ -1,6 +1,24 @@
 import 'package:cosmos_foundation/enumerators/cosmos_control_states.dart';
+import 'package:cosmos_foundation/models/structs/standard_theme_struct.dart';
+import 'package:cosmos_foundation/models/structs/states_control_theme_struct.dart';
 
-T evaluateControlState<T>(
+T evaluatePropertyState<T>(
+  CosmosControlStates state, {
+  T? onHover,
+  T? onSelect,
+  required T onIdle,
+}) {
+  switch (state) {
+    case CosmosControlStates.hovered:
+      return onHover ?? onIdle;
+    case CosmosControlStates.selected:
+      return onSelect ?? onIdle;
+    default:
+      return onIdle;
+  }
+}
+
+T evaluateBuildPropertyState<T>(
   CosmosControlStates state, {
   T Function()? onHover,
   T Function()? onSelect,
@@ -13,5 +31,19 @@ T evaluateControlState<T>(
       return onSelect?.call() ?? onIdle();
     default:
       return onIdle();
+  }
+}
+
+StandardThemeStruct evaluateThemeState(
+  CosmosControlStates state,
+  StateControlThemeStruct struct,
+) {
+  switch (state) {
+    case CosmosControlStates.hovered:
+      return struct.hoverStruct ?? struct.mainStruct;
+    case CosmosControlStates.selected:
+      return struct.selectStruct ?? struct.mainStruct;
+    default:
+      return struct.mainStruct;
   }
 }
