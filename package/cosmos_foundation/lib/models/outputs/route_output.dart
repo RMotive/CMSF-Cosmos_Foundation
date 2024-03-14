abf12e435064a43078e6a7e07eee1899dc809b67
Renato Urrea <usuaryrenato@hotmail.com>
2024-03-13 23:33:56 -0700

    added a way to access current page key on route output

--- package/cosmos_foundation/lib/models/outputs/route_output.dart
+++ package/cosmos_foundation/lib/models/outputs/route_output.dart
@@ -1,12 +1,15 @@
 import 'package:cosmos_foundation/helpers/route_driver.dart';
 import 'package:cosmos_foundation/models/options/route_options.dart';
+import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 
 class RouteOutput {
   final RouteOptions route;
   final String absolutePath;
+  final ValueKey<String>? pageKey;
 
   const RouteOutput({
+    this.pageKey,
     required this.route,
     required this.absolutePath,
   });
@@ -15,6 +18,7 @@ class RouteOutput {
 
     return RouteOutput(
       route: options,
+      pageKey: goState.pageKey,
       absolutePath: absolutePath ?? (goState.uri.toString()),
     );
   }
