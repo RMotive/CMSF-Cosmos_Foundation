3b0a6ff94f7e037eb76b105a24c6b5b4fdba43df
Renato Urrea <usuaryrenato@hotmail.com>
2024-03-27 22:39:40 -0700

    added a handler for custom controls

---
+++ package/cosmos_foundation/lib/foundation/simplifiers/control_handler.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ControlHandler extends StatelessWidget {
+  final void Function(bool)? onHover;
+  final VoidCallback? onClick;
+  final MouseCursor cursor;
+  final Widget? child;
+  const ControlHandler({
+    super.key,
+    this.cursor = MouseCursor.defer,
+    this.onClick,
+    this.onHover,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: cursor,
+      onEnter: onHover != null
+          ? (PointerEnterEvent event) {
+              onHover?.call(true);
+            }
+          : null,
+      onExit: onHover != null
+          ? (PointerExitEvent event) {
+              onHover?.call(false);
+            }
+          : null,
+      child: InkWell(
+        onTap: onClick,
+        child: child,
+      ),
+    );
+  }
+}
